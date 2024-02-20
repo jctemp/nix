c3ec6cac86a80516b4fd8e4b6a0aa1abc8364521
Jamie Temple <jamie-temple@live.de>
2024-02-20 23:54:52 +0100

    refactor(home): move dev. to user package

--- home/temple/default.nix
+++ home/temple/default.nix
@@ -12,6 +12,32 @@
       google-chrome
       keepassxc
       spotify
+
+      # Development
+      lldb
+      clang-tools
+      cmake-language-server
+      dockerfile-language-server-nodejs
+      gopls
+      haskell-language-server
+      marksman
+      nil
+      nodePackages.bash-language-server
+      nodePackages.typescript-language-server
+      nodePackages.vscode-langservers-extracted
+      ocamlPackages.dune_3
+      ocamlPackages.ocaml-lsp
+      ocamlPackages.reason
+      opam
+      python311Packages.python-lsp-server
+      rust-analyzer
+      swiProlog
+      taplo
+      texlab
+      texlab
+      typst-lsp
+      yaml-language-server
+      zls
     ];
     stateVersion = version;
   };
@@ -71,32 +97,6 @@
     helix = {
       enable = true;
       defaultEditor = true;
-      extraPackages = with pkgs; [
-        lldb
-        clang-tools
-        cmake-language-server
-        dockerfile-language-server-nodejs
-        gopls
-        haskell-language-server
-        marksman
-        nil
-        nodePackages.bash-language-server
-        nodePackages.typescript-language-server
-        nodePackages.vscode-langservers-extracted
-        ocamlPackages.dune_3
-        ocamlPackages.ocaml-lsp
-        ocamlPackages.reason
-        opam
-        python311Packages.python-lsp-server
-        rust-analyzer
-        swiProlog
-        taplo
-        texlab
-        texlab
-        typst-lsp
-        yaml-language-server
-        zls
-      ];
       settings = builtins.fromTOML (builtins.readFile ./settings/hx-settings.toml);
     };
 
