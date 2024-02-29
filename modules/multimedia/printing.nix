eb956e5f2ffc897ce6b5086a7e7fb8d58936e090
Jamie Temple <jamie-temple@live.de>
2024-02-29 14:57:18 +0100

    chore(printing): extend configuration

--- modules/multimedia/printing.nix
+++ modules/multimedia/printing.nix
@@ -1,6 +1,25 @@
 {pkgs, ...}: {
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
+
   services.printing = {
     enable = true;
     drivers = [pkgs.gutenprint];
+    browsing = true;
+    browsedConf = ''
+      BrowseDNSSDSubTypes _cups,_print
+      BrowseLocalProtocols all
+      BrowseRemoteProtocols all
+      CreateIPPPrinterQueues All
+
+      BrowseProtocols all
+    '';
   };
 }
