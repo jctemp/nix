f9b02f1e46882c413b1f3cad0b9237107983cc79
Jamie Temple <jamie-temple@live.de>
2024-03-15 09:21:58 +0100

    chore(system): update cornwall config

--- machines/cornwall/default.nix
+++ machines/cornwall/default.nix
@@ -1,4 +1,4 @@
-{lib, ...}: {
+{pkgs, lib, ...}: {
   imports = [./hardware-configuration.nix];
 
   hosts = {
@@ -18,4 +18,31 @@
       libvirt.enable = true;
     };
   };
+
+  boot = {
+    kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages;
+    supportedFilesystems = lib.mkForce ["btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" "zfs"];
+    initrd.postDeviceCommands = lib.mkAfter ''
+      zfs rollback -r rpool/local/root@blank
+    '';
+  };
+
+  time.hardwareClockInLocalTime = true;
+
+  services.zfs = {
+    autoScrub.enable = true;
+    autoSnapshot.enable = true;
+  };
+
+  environment.etc = {
+    "NetworkManager/system-connections" = {
+      source = "/persist/etc/NetworkManager/system-connections/";
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    # https://www.freedesktop.org/software/systemd/man/latest/tmpfiles.d.html
+    # create symlink to
+    "L /var/lib/bluetooth - - - - /persist/var/lib/bluetooth"
+  ];
 }
