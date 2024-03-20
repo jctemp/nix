bd98e9b2668870c6a3d14162a9274c2ded48768e
Jamie Temple <jamie-temple@live.de>
2024-03-20 12:30:38 +0100

    feat(nvidia): add cachix substitutions

--- modules/default.nix
+++ modules/default.nix
@@ -18,6 +18,7 @@
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
       trusted-users = ["root"];
+      substituters = ["https://cache.nixos.org/"];
     };
   };
 
