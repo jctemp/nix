9c6c9c9bbfac10f214dad8741b5dfdaf1e5c2cf0
Jamie Temple <jamie-temple@live.de>
2024-10-02 14:20:51 +0200

    chore(project): reduce dependencies

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
           };
           stateVersion = "23.11";
           modules = [
-            nix-hardware.nixosModules.microsoft-surface-common
+            #nix-hardware.nixosModules.microsoft-surface-common
           ];
         })
         (lib.mkHost {
