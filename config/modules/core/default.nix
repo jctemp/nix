72a82d544e13ec0e2d0bb36da6a07690a250ce97
Jamie Temple <jamie.c.temple@gmail.com>
2025-02-26 19:15:27 +0100

    Revert "feat(modules): add nix community substituters"
    This reverts commit f6772d1c3a33376f22782669f97c49cbc217113a.

--- config/modules/core/default.nix
+++ config/modules/core/default.nix
@@ -22,13 +22,6 @@
       auto-optimise-store = true;
       keep-outputs = true;
       trusted-users = ["@wheel"];
-
-      nix.settings.substituters = [
-        "https://nix-community.cachix.org"
-      ];
-      nix.settings.trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
     };
   };
 
